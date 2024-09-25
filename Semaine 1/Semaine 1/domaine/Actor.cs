6f2bff5109134ea22b1da933a4f0479149637712
IweZix <lucanclv2@gmail.com>
2024-09-25 19:55:00 +0200

    add: semaine 1

---
+++ Semaine 1/Semaine 1/domaine/Actor.cs
@@ -0,0 +1,50 @@
+﻿namespace Semaine_1.domaine
+{
+    [Serializable]
+    internal class Actor : Person
+    {
+        private static readonly long _serialVersionUID = 1L;
+        private readonly int _sizeInCentimeter;
+        private List<Movie> _movies;
+
+        public Actor(string name, string firstname, DateTime birthDate, int sizeInCentimeter) 
+            : base(name, firstname, birthDate)
+        {
+            _sizeInCentimeter = sizeInCentimeter;
+            _movies = new List<Movie> ();
+        }
+
+        public bool AddMovie(Movie? movie)
+        {
+            if (movie == null || _movies.Contains(movie))
+            {
+                return false;
+            }
+
+            if (!movie.ContainsActor(this))
+            {
+                movie.AddActor(this);
+            }
+
+            _movies.Add(movie);
+
+            return true;
+        }
+
+        public bool ContainsMovie(Movie movie)
+        {
+            return _movies.Contains(movie);
+        }
+
+        public IEnumerator<Movie> MovieEnumerator()
+        {
+            return _movies.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return "Actor [name = " + Name + ", firstname = " + Firstname + ", sizeInCentimeter = " +
+                   _sizeInCentimeter + ", birthdate = " + BirthDate + "]";
+        }
+    }
+}
---
+++ Semaine 1/Semaine 1/domaine/Director.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semaine_1.domaine
+{
+    internal class Director : Person
+    {
+        private static readonly long _serialVersionUID = 5952964360274024205L;
+        private IList<Movie> _directedMovies;
+
+        public Director(string name, string firstname, DateTime birtDate)
+            : base(name, firstname, birtDate)
+        {
+            _directedMovies = new List<Movie>();
+        }
+
+        public bool AddMovie(Movie movie)
+        {
+            if (_directedMovies.Contains(movie))
+            {
+                return false;
+            }
+
+            movie.Director ??= this;
+
+            _directedMovies.Add((movie));
+
+            return true;
+        }
+
+        public IEnumerator<Movie> Movies()
+        {
+            return _directedMovies.GetEnumerator();
+        }
+    }
+}
