0241e48f25f3a1f1990de4a66767d11471b42c8f
IweZix <IweZix@users.noreply.github.com>
2024-10-10 09:01:18 +0200

    refactor: S4

---
+++ Semaine 4/Semaine 4/client/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Semaine_4.client
+{
+    // 4.	Créez une classe Logger
+    class Logger
+    {
+        public delegate void Log(String s);
+
+        public Log log;
+
+        public void LogMessage(String s)
+        {
+            log?.Invoke(s);
+        }
+    }
+
+    // 5.	Créez une classe ConsoleLogger qui implémente un logger qui écrit sur la Console
+    class ConsoleLogger
+    {
+        public void LogMessage(String s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    // 6.	Créez une classe FileLogger qui implémente un logger qui écrit dans un fichier
+    class FileLogger
+    {
+        public void LogMessage(String s)
+        {
+            File.WriteAllText("log.txt", s);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            logger.log += new ConsoleLogger().LogMessage;
+            logger.log += new FileLogger().LogMessage;
+
+            logger.LogMessage("test");
+
+        }
+    }
+}
